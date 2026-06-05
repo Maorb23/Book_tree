5c29788ea28aa357a696879723591e84bb9fb83c
Maorb23 <maorblumberg@gmail.com>
2026-06-05 17:58:58 +0300

    last changes

--- book_tree/urls.py
+++ book_tree/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # Admin site, this url is for site administrators to manage the application
     path('', include('tree.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
